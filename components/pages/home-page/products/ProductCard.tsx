4fa07c782fb068ed5a2fd0634e447790129b3e0d
OsssGhoul <osamalghoul2@gmail.com>
2025-10-06 19:22:18 +0300

    Screw this

--- components/pages/home-page/products/ProductCard.tsx
+++ components/pages/home-page/products/ProductCard.tsx
@@ -1,5 +1,5 @@
 import { Heart, Eye, Star } from "lucide-react";
-import React, { useState } from "react";
+import { useState } from "react";
 
 export default function ProductCard({
   new: isNew = false,
