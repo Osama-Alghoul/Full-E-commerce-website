338a0a802b4707b4255bcc5806e5a4b2b5d28338
OsssGhoul <osamalghoul2@gmail.com>
2025-11-11 10:57:03 +0200

    Feat: Added category links in the home page "Aside & Categories section"

--- src/components/pages/home-page/categories/CategoriesCards.tsx
+++ src/components/pages/home-page/categories/CategoriesCards.tsx
@@ -1,30 +1,41 @@
 import {
   Smartphone,
-  Monitor,
-  Camera,
+  Shirt,
+  ForkKnifeCrossed,
+  Sofa,
   Headphones,
-  Gamepad,
   Watch,
 } from "lucide-react";
 import OutlineCard from "../../../ui/OutlineCard";
+import { Link } from "react-router";
 
 export default function CategoriesCards() {
   return (
-      <div
-        className="flex gap-[30px] py-16 overflow-x-scroll justify-center snap-x snap-mandatory"
-      >
-        <OutlineCard
-          name="Phones"
-          icon={<Smartphone className="h-10 w-9" />}
-        />
-        <OutlineCard name="Monitors" icon={<Monitor className="h-10 w-9" />} />
+    <div className="flex gap-[30px] py-16 overflow-x-scroll justify-start snap-x snap-mandatory">
+      <Link to={`/products?category=home-decoration`}>
+        <OutlineCard name="Decoration" icon={<Sofa className="h-10 w-9" />} />
+      </Link>{" "}
+      <Link to={`/products?category=mens-watches`}>
         <OutlineCard name="SmartWatch" icon={<Watch className="h-10 w-9" />} />
-        <OutlineCard name="Cameras" icon={<Camera className="h-10 w-9" />} />
+      </Link>
+      <Link to={`/products?category=smartphones`}>
+        <OutlineCard name="Phones" icon={<Smartphone className="h-10 w-9" />} />
+      </Link>
+      <Link to={`/products?category=kitchen-accessories`}>
+        <OutlineCard
+          name="Kitchen"
+          icon={<ForkKnifeCrossed className="h-10 w-9" />}
+        />
+      </Link>
+      <Link to={`/products?category=mobile-accessories`}>
         <OutlineCard
           name="Headphones"
           icon={<Headphones className="h-10 w-9" />}
         />
-        <OutlineCard name="Gamepads" icon={<Gamepad className="h-10 w-9" />} />
-      </div>
+      </Link>
+      <Link to={`/products?category=tops`}>
+        <OutlineCard name="Tops" icon={<Shirt className="h-10 w-9" />} />
+      </Link>
+    </div>
   );
 }
--- src/components/pages/home-page/hero-section/Aside.tsx
+++ src/components/pages/home-page/hero-section/Aside.tsx
@@ -6,40 +6,34 @@ export default function PageAsideAside() {
     <ul className="list-none border-r border-gray-300 py-8 w-56 hidden md:block [&>li]:hover:text-primary">
       <HeaderDrawer title="Woman's Fashion">
         <li>
-          <Link to="#">Accessories</Link>
+          <Link to="/products?category=womens-dresses">Dresses</Link>
         </li>
         <li>
-          <Link to="#">Jewelries</Link>
+          <Link to="/products?category=womens-jewellery">Jewelries</Link>
         </li>
       </HeaderDrawer>
       <HeaderDrawer title="Men's Fashion">
         <li>
-          <Link to="#">Sports</Link>
+          <Link to="/products?category=mens-shirts">Sports</Link>
         </li>
         <li>
-          <Link to="#">Luxury</Link>
+          <Link to="/products?category=mens-watches">Luxury</Link>
         </li>
       </HeaderDrawer>
       <li>
-        <Link to="#">Electronics</Link>
+        <Link to="/products?category=mobile-accessories">Electronics</Link>
       </li>
       <li>
-        <Link to="#">Home & Lifestyle</Link>
+        <Link to="/products?category=home-decoration">Home & Lifestyle</Link>
       </li>
       <li>
-        <Link to="#">Medicine</Link>
+        <Link to="/products?category=sports-accessories">Sports & Outdoor</Link>
       </li>
       <li>
-        <Link to="#">Sports & Outdoor</Link>
+        <Link to="/products?category=groceries">Groceries</Link>
       </li>
       <li>
-        <Link to="#">Baby's & Toys</Link>
-      </li>
-      <li>
-        <Link to="#">Groceries & Pets</Link>
-      </li>
-      <li>
-        <Link to="#">Health & Beauty</Link>
+        <Link to="/products?category=beauty">Beauty</Link>
       </li>
     </ul>
   );
