c2d4073e1770c0b3cb2b2b692bde7eacba8a3b2b
OsssGhoul <osamalghoul2@gmail.com>
2025-11-12 13:23:55 +0200

    Delete: the custom checkout page, now we use stripe checkout page

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,6 @@ import HomePage from "./components/HomePage";
 import Login from "./components/pages/log-in/Login";
 import SignUp from "./components/pages/sign-up/signUp";
 import Cart from "./components/pages/cart/cart";
-import CheckOut from "./components/pages/checkout/checkout";
 import ProductDetails from "./components/pages/product-details/ProductDetails";
 import About from "./components/pages/about/about";
 import Contact from "./components/pages/contact/contact";
@@ -28,16 +27,7 @@ const router = createBrowserRouter([
           { path: "register", Component: SignUp },
         ],
       },
-      {
-        path: "cart",
-        children: [
-          { index: true, Component: Cart },
-          {
-            path: "checkout",
-            Component: CheckOut,
-          },
-        ],
-      },
+      { path: "cart", Component: Cart },
       {
         path: "products",
         children: [
